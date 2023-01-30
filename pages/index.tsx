87c261ca76b9a59202b942443912227a9026d50d
Jadon Knight <Jadon.Knight@outlook.com>
2023-01-30 22:46:25 +1100

    Install and config tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-sky-400/100">
+        Hello world!
+      </h1>
+    </>
+  );
 }
