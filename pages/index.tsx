ed0bcea986af1736a6c51c412594d2b2423ff627
Jadon Knight <Jadon.Knight@outlook.com>
2023-02-06 23:01:22 +1100

    Coming soon section added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,52 @@ import { useSession } from "next-auth/react";
 import WorkoutFab from "../components/buttons/workout-fab";
 
 export default function Home() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const username = session?.user?.name;
 
-  return (
-    <>
-      {username ?
+  const authenticated = status === "authenticated";
+
+  if (authenticated) {
+    return (
+      <>
         <h1 className="text-2xl font-bold">Welcome {username}</h1>
-        :
-        <h1 className="text-2xl font-bold">Welcome</h1>
-      }
-      <WorkoutFab />
-    </>
-  );
+        <WorkoutFab />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <section
+          className="h-screen bg-cover bg-teal-500"
+        >
+          <div className="flex pt-32 w-full items-center justify-center container mx-auto px-8">
+            <div className="max-w-2xl text-center">
+              <h1 className="text-3xl sm:text-5xl capitalize tracking-widest text-white lg:text-7xl">
+                Coming Soon
+              </h1>
+
+              {/* <p className="mt-6 lg:text-lg text-white">
+                You can subscribe to our newsletter, and let you know when we
+                are back
+              </p> */}
+
+              {/* TODO: Add This */}
+              {/* <div className="mt-8 flex flex-col space-y-3 sm:-mx-2 sm:flex-row sm:justify-center sm:space-y-0">
+                <input
+                  id="email"
+                  type="text"
+                  className="rounded-md border border-transparent bg-white/20 px-4 py-2 text-white placeholder-white backdrop-blur-sm focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 sm:mx-2"
+                  placeholder="Email Address"
+                />
+
+                <button className="transform rounded-md bg-blue-700 px-8 py-2 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-200 hover:bg-blue-600 focus:bg-blue-600 focus:outline-none sm:mx-2">
+                  Notify Me
+                </button>
+              </div> */}
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
