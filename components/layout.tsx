7ca6c66b5312ac8e548b9caf9384a949a8facca9
Jadon Knight <Jadon.Knight@outlook.com>
2023-06-16 12:39:01 +1000

    Basic footer added

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,6 +7,27 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <Navbar />
       </div>
       <main className="flex-grow bg-gray-100">{children}</main>
+      <footer className="bg-gray-200 py-4 text-center">
+        <p className="text-sm text-gray-600">
+          This app is currently in alpha stage. &copy; Jadon Knight 2022-
+          {new Date().getFullYear()}
+        </p>
+        <div className="mt-2">
+          <a
+            href="mailto:jadon.knight@outlook.com"
+            className="text-blue-500 underline"
+          >
+            Report Bugs and Issues
+          </a>
+          <span className="mx-2 text-gray-500">|</span>
+          <a
+            href="https://github.com/JadonKnight/basic-workout-tracker/issues"
+            className="text-blue-500 underline"
+          >
+            GitHub Issues
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
