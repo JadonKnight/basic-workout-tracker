2978c0a12ee5ce6c74e976429c54498c1dca5c42
Jadon Knight <Jadon.Knight@outlook.com>
2023-07-27 14:53:53 +1000

    Arrows to dashboard buttons

--- next-app/app/dashboard/page.tsx
+++ next-app/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import fetchServerSideSession from "@/lib/fetchServerSideSession";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
 
 export default async function Page() {
   const session = await fetchServerSideSession();
@@ -10,7 +11,10 @@ export default async function Page() {
       <div className="container mx-auto">
         <div className="flex flex-col justify-start">
           <h2 className="text-4xl font-bold text-white mb-4">
-            <span className="text-white">Welcome </span><span className="text-amber-400">{user?.name ? `${user.name}` : ""}</span>
+            <span className="text-white">Welcome </span>
+            <span className="text-amber-400">
+              {user?.name ? `${user.name}` : ""}
+            </span>
           </h2>
           <p className="text-lg text-white mb-8">
             Start tracking and improving your workouts now.
@@ -19,21 +23,24 @@ export default async function Page() {
         <div className="md:space-x-4 space-y-4 md:space-y-0 flex justify-start flex-col md:flex-row">
           <Link
             href="/workouts/new"
-            className="bg-white text-blue-500 hover:bg-blue-600 hover:text-white px-6 py-3 rounded-md font-semibold"
+            className="flex justify-between items-center bg-white text-blue-500 hover:bg-blue-600 hover:text-white px-6 py-3 rounded-md font-semibold"
           >
             Create New Workout
+            <ArrowRightIcon height={32} width={32} />
           </Link>
           <Link
             href="/workouts"
-            className="bg-white text-green-500 hover:bg-green-600 hover:text-white px-6 py-3 rounded-md font-semibold"
+            className="flex justify-between items-center bg-white text-green-500 hover:bg-green-600 hover:text-white px-6 py-3 rounded-md font-semibold"
           >
             Your Workouts
+            <ArrowRightIcon height={32} width={32} />
           </Link>
           <Link
             href="/history"
-            className="bg-white text-indigo-500 hover:bg-indigo-600 hover:text-white px-6 py-3 rounded-md font-semibold"
+            className="flex justify-between items-center bg-white text-indigo-500 hover:bg-indigo-600 hover:text-white px-6 py-3 rounded-md font-semibold"
           >
             Workout History
+            <ArrowRightIcon height={32} width={32} />
           </Link>
         </div>
       </div>
