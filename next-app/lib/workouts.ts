445c32c4a18f6c7b4714727bbc1d408ba36176e6
Jadon Knight <Jadon.Knight@outlook.com>
2023-08-01 10:14:57 +1000

    Modified fetchWorkout to support passing to client components

--- next-app/lib/workouts.ts
+++ next-app/lib/workouts.ts
@@ -4,27 +4,44 @@ export default async function fetchWorkouts(userId: number) {
   const workouts = await prisma.workout.findMany({
     where: {
       userId,
-      deletedAt: null
-    }
+      deletedAt: null,
+    },
   });
 
   return workouts;
 }
 
+// This function is different to above because I use its return value
+// to pass from a server component to client and we can't have Date objects.
+// This seems more sensible than writing filter functions on the client.
+
+// If I need dates I can always write additions here to include them in.
 export async function fetchWorkout(userId: number, workoutId: number) {
   const workout = await prisma.workout.findFirst({
     where: {
       userId,
       deletedAt: null,
-      id: workoutId
+      id: workoutId,
     },
-    include: {
+    select: {
+      id: true,
+      name: true,
+      userId: true,
+      daysOfWeek: true,
       workoutExercise: {
-        include: {
-          exercise: true
-        }
-      }
-    }
+        select: {
+          workoutId: true,
+          exercise: {
+            select: {
+              id: true,
+              description: true,
+              name: true,
+              createdById: true,
+            },
+          },
+        },
+      },
+    },
   });
 
   return workout;
